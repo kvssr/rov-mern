97fbd24eaa9859e588e7530ce2b63447fef537be
kvssr <kevin.visser.info@gmail.com>
2023-07-03 10:41:58 +0200

    Added Raidselector component

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,29 +1,19 @@
 import React, { useState } from "react";
 import FlexBetween from "components/FlexBetween";
 import Header from "components/Header";
-import {
-  Box,
-  Typography,
-  useTheme,
-  useMediaQuery,
-  Select,
-  MenuItem,
-} from "@mui/material";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import RaidsChart from "components/RaidsChart";
 import { useGetRaidsInfoListQuery } from "state/api";
+import RaidSelector from "components/RaidSelector";
 
 const Dashboard = () => {
   const theme = useTheme();
   const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
   const { data: raidInfoList, isLoading } = useGetRaidsInfoListQuery();
-  const [selectedRaid, setSelectedRaid] = useState("");
+  const [selectedRaid, setSelectedRaid] = useState(-1);
   if (!raidInfoList || isLoading || raidInfoList.length === 0) {
     return "Is Loading...";
   }
-
-  const handleRaidSelect = (id) => {
-    setSelectedRaid(id);
-  };
 
   const statItems = [
     {
@@ -105,27 +95,11 @@ const Dashboard = () => {
         />
       </FlexBetween>
       <FlexBetween>
-        <Box>
-          <Select
-            value={selectedRaid ? selectedRaid : raidInfoList[0]["id"]}
-            label="Raid"
-            onChange={(e) => handleRaidSelect(e.target.value)}
-          >
-            {raidInfoList.map((raid) => {
-              const start_date = raid["start_date"].split("T")[0];
-              const start_time = raid["start_time"].split("T")[1].split(".")[0];
-              const end_time = raid["end_time"].split("T")[1].split(".")[0];
-              const text = `${start_date} | ${start_time} - ${end_time}`;
-              return (
-                <MenuItem
-                  value={raid["id"]}
-                  key={raid["id"]}
-                >
-                  {text}
-                </MenuItem>
-              );
-            })}
-          </Select>
+        <Box sx={{ mt: "1rem" }}>
+          <RaidSelector
+            selectedRaid={selectedRaid}
+            setSelectedRaid={setSelectedRaid}
+          />
         </Box>
       </FlexBetween>
 
--- client/src/scenes/raids/index.jsx
+++ client/src/scenes/raids/index.jsx
@@ -10,6 +10,7 @@ import {
 import Header from "components/Header";
 import RaidsChart from "components/RaidsChart";
 import { useGetRaidsInfoListQuery } from "state/api";
+import RaidSelector from "components/RaidSelector";
 
 const statItems = [
   {
@@ -59,18 +60,15 @@ const statItems = [
 ];
 
 const Raids = () => {
+  const [selectedRaid, setSelectedRaid] = useState(-1);
   const [view, setView] = useState("dmg");
   const [max, setMax] = useState(15);
   const { data: raidInfoList, isLoading } = useGetRaidsInfoListQuery();
-  const [selectedRaid, setSelectedRaid] = useState("");
 
   if (!raidInfoList || isLoading) {
     return "Is Loading...";
   }
 
-  const handleRaidSelect = (id) => {
-    setSelectedRaid(id);
-  };
   return (
     <Box
       display="grid"
@@ -84,68 +82,50 @@ const Raids = () => {
         title="Overview"
         subtitle="Overview of Raid"
       />
-      <Box height="80vh">
-        <FormControl sx={{ m: "1rem" }}>
-          <InputLabel>Raid</InputLabel>
-          <Select
-            value={selectedRaid ? selectedRaid : raidInfoList[0]["id"]}
-            label="Raid"
-            onChange={(e) => handleRaidSelect(e.target.value)}
-          >
-            {raidInfoList.map((raid) => {
-              // const info = raid["overall_raid_stats"];
-              const start_date = raid["start_date"].split("T")[0];
-              const start_time = raid["start_time"].split("T")[1].split(".")[0];
-              const end_time = raid["end_time"].split("T")[1].split(".")[0];
-              const text = `${start_date} | ${start_time} - ${end_time}`;
-              return (
-                <MenuItem
-                  value={raid["id"]}
-                  key={raid["id"]}
-                >
-                  {text}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <FormControl sx={{ mt: "1rem" }}>
-          <InputLabel>Stat</InputLabel>
-          <Select
-            value={view}
-            label="View"
-            onChange={(e) => setView(e.target.value)}
-          >
-            {statItems.map(({ text, value }) => {
-              return (
-                <MenuItem
-                  value={value}
-                  key={value}
-                >
-                  {text}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <FormControl sx={{ mt: "1rem", ml: "1rem", width: "100px" }}>
-          <Slider
-            id="silder-max"
-            size="medium"
-            aria-label="Max"
-            defaultValue={15}
-            // value={max}
-            valueLabelDisplay="auto"
-            step={1}
-            marks={[
-              { value: 5, label: 5 },
-              { value: 50, label: 50 },
-            ]}
-            min={5}
-            max={50}
-            onChangeCommitted={(_, value) => setMax(value)}
+      <Box height="75vh">
+        <Box sx={{ mt: "1rem" }}>
+          <RaidSelector
+            selectedRaid={selectedRaid}
+            setSelectedRaid={setSelectedRaid}
           />
-        </FormControl>
+          <FormControl sx={{ ml: "1rem" }}>
+            <InputLabel>Stat</InputLabel>
+            <Select
+              value={view}
+              label="View"
+              onChange={(e) => setView(e.target.value)}
+            >
+              {statItems.map(({ text, value }) => {
+                return (
+                  <MenuItem
+                    value={value}
+                    key={value}
+                  >
+                    {text}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ ml: "1rem", width: "100px" }}>
+            <Slider
+              id="silder-max"
+              size="medium"
+              aria-label="Max"
+              defaultValue={15}
+              // value={max}
+              valueLabelDisplay="auto"
+              step={1}
+              marks={[
+                { value: 5, label: 5 },
+                { value: 50, label: 50 },
+              ]}
+              min={5}
+              max={50}
+              onChangeCommitted={(_, value) => setMax(value)}
+            />
+          </FormControl>
+        </Box>
         <RaidsChart
           view={view}
           raid_id={selectedRaid ? selectedRaid : raidInfoList[0]["id"]}
